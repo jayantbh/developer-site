704cf00eea88413e8116d3df57b920a8f7aee6fc
Jayant Bhawal <bhawal.jayant@gmail.com>
2021-05-02 18:27:56 +0530

    Add an element for a portal.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,10 @@
+import React from 'react';
 import { wrapRootElement as wrap } from './src/wrap-root-element';
 
 export const wrapRootElement = wrap;
+
+export const onRenderBody = ({setPostBodyComponents}) => {
+    setPostBodyComponents([
+        <div id='application-portal' />
+    ]);
+}
