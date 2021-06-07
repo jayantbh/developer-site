0798c3dcad0ba597126cee5f72ab050cd19fc946
Jayant Bhawal <bhawal.jayant@gmail.com>
2021-06-08 03:46:45 +0530

    Update generic presentation components to match new fonts and design

--- src/components/InlineImages/index.tsx
+++ src/components/InlineImages/index.tsx
@@ -1,5 +1,6 @@
 import { Lightbox } from 'components/Lightbox';
 import React, { FC, HTMLAttributes, useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
 
 import * as css from './styles.module.scss';
 
@@ -14,27 +15,32 @@ type Props = {
   images: (URL | ImgRes)[];
 } & HTMLAttributes<HTMLElement>;
 
-const InlineImages: FC<Props> = ({ images, height, ...props }) => {
-  const [lightboxedImg, setLightboxedImg] = useState<ImgRes | void>();
-  return (
-    <>
-      {lightboxedImg && (
-        <Lightbox src={lightboxedImg.url} caption={lightboxedImg.caption} onClose={() => setLightboxedImg()} />
-      )}
-      <section {...props} className={`${css.container} ${props.className || ''}`}>
-        {images.map((img) => {
-          const url = typeof img === 'string' ? img : img.url;
-          const caption = typeof img === 'string' ? undefined : img.caption;
+const LightboxWrapper = styled.div`
+  max-width: 100%;
+`;
 
-          return (
-            <div key={url} className={caption && css.captionImg}>
-              <img src={url} height={height} onClick={() => setLightboxedImg({ url, caption })} />
-              {caption && <span className={css.caption}>{caption}</span>}
-            </div>
-          );
-        })}
-      </section>
-    </>
+const InlineImages: FC<Props> = ({ images, height, ...props }) => {
+  return (
+    <section {...props} className={`${css.container} ${props.className || ''}`}>
+      {images.map((img) => {
+        const url = typeof img === 'string' ? img : img.url;
+        const caption = typeof img === 'string' ? undefined : img.caption;
+
+        return (
+          <LightboxWrapper
+            key={url}
+            className={caption && css.captionImg}
+            style={{
+              width: `calc(${100 / images.length}% - ${
+                (8 * (images.length - 1)) / images.length
+              }px)`,
+            }}
+          >
+            <Lightbox src={url} height={height} caption={caption} />
+          </LightboxWrapper>
+        );
+      })}
+    </section>
   );
 };
 
--- src/components/Lightbox/index.tsx
+++ src/components/Lightbox/index.tsx
@@ -1,8 +1,23 @@
-import React, { useRef, useEffect, MouseEventHandler, useMemo } from 'react';
+import { HotkeyHandler } from 'components/HotkeyHandler';
+import { HTMLMotionProps, motion } from 'framer-motion';
+import React, {
+  useRef,
+  useEffect,
+  MouseEventHandler,
+  useMemo,
+  useState,
+  CSSProperties,
+  HTMLAttributes,
+  FC,
+} from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import * as css from './styles.module.scss';
 
-const portalEl = typeof document !== `undefined` ? document.getElementById('application-portal') : null;
+const portalEl =
+  typeof document !== `undefined`
+    ? document.getElementById('application-portal')
+    : null;
 
 const swallowClicks: MouseEventHandler = (e) => {
   e.preventDefault();
@@ -13,40 +28,135 @@ type LightboxProps = {
   src: string;
   caption?: string;
   onClose?: () => void;
+} & HTMLMotionProps<'img'>;
+
+const Img = styled(motion.img)`
+  max-height: 90vh;
+  max-width: 90vw;
+  position: relative;
+  margin: 0;
+  overflow: hidden;
+  border-radius: 8px;
+`;
+
+const LightboxContent = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  min-height: 100px;
+  overflow: hidden;
+  border-radius: 8px;
+`;
+
+const Caption = styled(motion.div)<{ maximized: boolean }>`
+  position: absolute;
+  bottom: ${(p) => (p.maximized ? '2em' : '1em')};
+  margin: auto;
+  background: #fffd;
+  border-radius: 100vw;
+  padding: 4px 16px;
+  font-size: ${(p) => (p.maximized ? '1em' : '0.7em')};
+`;
+
+const Close = styled(motion.div)`
+  position: absolute;
+  top: 1em;
+  margin: auto;
+  background: #fffd;
+  border-radius: 100vw;
+  padding: 4px 16px;
+  font-size: 0.7em;
+`;
+
+const Backdrop = styled(motion.div)`
+  background-color: #8885;
+  backdrop-filter: blur(4px);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const lightboxStyles: CSSProperties = {
+  position: 'fixed',
+  top: '0',
+  left: '0',
+  right: '0',
+  bottom: '0',
+  margin: 'auto',
 };
 
-export const Lightbox = ({ src, caption, onClose }: LightboxProps) => {
-  const elRef = useRef(document.createElement('div'));
-  console.log(src);
-
-  useEffect(() => {
-    const el = elRef.current;
-    console.log(el, portalEl);
-    if (!el || !portalEl || !src) return;
-
-    portalEl.appendChild(el);
-
-    return () => {
-      const el = elRef.current;
-      if (!el || !portalEl) return;
-
-      portalEl.removeChild(el);
-    };
-  }, [src]);
-
-  const children = useMemo(
-    () => (
-      <div className={css.lightbox} onClick={onClose}>
-        <button className={css.close}>CLOSE</button>
-        <img src={src} alt='Lightboxed image' onClick={swallowClicks} />
-        {caption && (
-          <span className={css.caption} onClick={swallowClicks}>
-            {caption}
-          </span>
-        )}
-      </div>
-    ),
-    [src, caption]
+export const Lightbox = ({
+  src,
+  caption,
+  onClose,
+  ...props
+}: LightboxProps) => {
+  const [maximized, setMaximized] = useState(false);
+  const [size, setSize] = useState<Record<'height' | 'width', string | number>>(
+    { height: 'auto', width: 'auto' }
   );
-  return ReactDOM.createPortal(children, elRef.current);
+
+  const toggleMaximized = () => {
+    setMaximized(!maximized);
+  };
+
+  return (
+    <div style={{ height: size.height, width: size.width, maxWidth: '100%' }}>
+      <LightboxContent
+        layout
+        style={maximized ? lightboxStyles : {}}
+        animate={{
+          zIndex: maximized ? 100 : 0,
+          transition: {
+            delay: maximized ? 0 : 1,
+          },
+        }}
+      >
+        <Backdrop layout />
+        <Img
+          {...props}
+          layout
+          ref={(el) => {
+            if (!el?.complete) return;
+            setSize({
+              height: el?.parentElement?.clientHeight || '0',
+              width: el?.parentElement?.clientWidth || '0',
+            });
+          }}
+          onLoad={(e) => {
+            const el = (e.target as HTMLImageElement | null)?.parentElement;
+            setSize({
+              height: el?.clientHeight || '0',
+              width: el?.clientWidth || '0',
+            });
+          }}
+          src={src}
+          height={maximized ? undefined : props.height}
+          onClick={toggleMaximized}
+        />
+        {!!caption && (
+          <Caption layout maximized={maximized}>
+            {caption}
+          </Caption>
+        )}
+        {maximized && (
+          <Close>
+            Close
+            <HotkeyHandler
+              hotkey='Escape'
+              onHotkey={toggleMaximized}
+              disabled={!maximized}
+              style={{
+                bottom: '-0.5em',
+                right: '-1em',
+              }}
+            />
+          </Close>
+        )}
+      </LightboxContent>
+    </div>
+  );
 };
