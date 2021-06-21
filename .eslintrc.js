f9f6b9f500d0d597544fcd4daa9906b71186deb2
Jayant Bhawal <bhawal.jayant@gmail.com>
2021-06-22 02:35:13 +0530

    Fix eslint parser version

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 8,
+    ecmaVersion: 2018,
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
       jsx: true,
